fb6f1e6337a2cb6684bffc1fd3ca4e5b3a18802c
Vardhan Patil <demonslayervardhan@gmail.com>
2023-07-26 16:58:11 +0530

    Change fonts and colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'display': ["monogram", "monospace"]
+			'display': ["Tektur", "monospace"],
+			'body': ["Roboto Flex", "sans-serif"]
 		},
 		extend: {},
 	},
